ea5764a10a3733cc12e064a9ad8414c549fae38b
sheshmani <sheshmanisahu09@gmail.com>
2026-05-09 01:07:19 +0530

    started the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,47 @@
-export const  Hero = () => {
-    return <section></section>
-}
+export const Hero = () => {
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Bg  */}
+      <div className="absolute inset-0">
+        <img
+          src="/src/assets/hero-bg.jpg"
+          alt="main-background"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background" />
+      </div>
+
+      {/* Green dots */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(30)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            style={{
+              backgroundColor: "#20B2A6",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animation: `slow-drift ${15+ Math.random() * 20}s ease-in-out infinite`,
+              animationDelay: `${Math.random()* 5}s`,
+            }}
+          />
+        ))}
+      </div>
+
+      {/* Content  */}
+
+        <div>
+            <div>
+                {/* Left Column text content */}
+                <div>
+
+                </div>
+                {/* Right profile image */}
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+
+    </section>
+  );
+};
